5e45ff75b8969e8334f3b6df9c4d8728cae07a0a
okularnik123 <okularnik7a@gmail.com>
2024-06-10 20:04:23 +0200

    Created writing logic

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState([]);
+  const [previousNumber, setPreviousNumber] = useState(0);
+  const [operator, setOperator] = useState("");
+
+  function addNumber(number) {
+    setCurrentNumber([...currentNumber, number]);
+  }
+
   return (
     <div className="calculatorGrid">
       <div className="display">
         <p className="lastOperation"></p>
-        <p className="operation"></p>
+        <p className="operation">{currentNumber}</p>
       </div>
       <button className="spanTwo">AC</button>
       <button>DEL</button>
       <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>1</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>2</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>3</button>
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>4</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>5</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>6</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>+</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>7</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>8</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>9</button>
       <button>-</button>
       <button>.</button>
-      <button>0</button>
+      <button onClick={(e) => addNumber(e.target.innerText)}>0</button>
       <button className="spanTwo">=</button>
     </div>
   );
